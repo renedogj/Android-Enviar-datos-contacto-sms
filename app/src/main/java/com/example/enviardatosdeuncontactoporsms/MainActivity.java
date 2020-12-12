17ba33aef181c24f95fe07a5985a10b1b22fc4e4
Javier <renedogj@gmail.com>
2020-12-12 18:33:33 +0100

    Nombre cambiado

--- app/src/main/java/com/example/enviardatosdeuncontactoporsms/MainActivity.java
+++ app/src/main/java/com/example/enviardatosdeuncontactoporsms/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.enviardatosdeuncontactoporsms;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
